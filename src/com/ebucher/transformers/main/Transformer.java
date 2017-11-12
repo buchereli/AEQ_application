d1cc72692b91b8f1348d350e07b177db7927eeca
buchereli <buchereli@gmail.com>
2017-11-11 16:35:01 -0800

    Outline with some methods completed

--- src/com/ebucher/transformers/main/Transformer.java
+++ src/com/ebucher/transformers/main/Transformer.java
@@ -4,7 +4,7 @@ package com.ebucher.transformers.main;
  * Created by buche on 11/10/2017.
  * Object representation of a transformer
  */
-public class Transformer {
+public class Transformer implements Comparable<Transformer> {
 
     // Input keys
     private final int NAME = 0, TEAM = 1;
@@ -16,16 +16,39 @@ public class Transformer {
     private final int STRENGTH = 0, RANK = 4, COURAGE = 5;
 
     // Takes an input String and builds a transformer representation
-    public Transformer(String input) {
-        String[] inputArray = input.split(",");
+    public Transformer(String input) throws Exception {
+        // Remove spaces
+        input = input.replace(" ", "");
+
+        // Make sure the input uses the correct format
+        if (!input.matches("^([a-z]|[A-Z])+,.*$"))
+            throw new Exception("Bad formatting! Transformer " + input + " does not start with a valid name");
+        if (!input.matches("^.*,([aAdD]|([aA]utobots)|([dD]ecepticons)),.*$"))
+            throw new Exception("Bad formatting! Transformer " + input + " does not have a valid team");
+        if (!input.matches("^.*(,(10|[1-9])){7}$"))
+            throw new Exception("Bad formatting! Transformer " + input + " does not have valid specs");
+
+        // Build the transformer
+        String[] inputArray = input.split("[,:]");
         name = inputArray[NAME];
-        team = inputArray[TEAM];
+        team = inputArray[TEAM].matches("([aA]|[aA]utobots)") ? "Autobots" : "Decepticons";
 
         for (int i = 0; i < specs.length; i++)
             specs[i] = Integer.parseInt(inputArray[i + 2]);
     }
 
+    // Allows transformers to be easily sorted by rank
+    @Override
+    public int compareTo(Transformer transformer) {
+        return 0;
+    }
 
+    public String getTeam() {
+        return team;
+    }
 
-
+    // Returns 1 if this transformer beats the input transformer, 0 for tie and -1 for lose
+    public int fight(Transformer transformer) {
+        return 0;
+    }
 }
--- src/com/ebucher/transformers/main/TransformerBattle.java
+++ src/com/ebucher/transformers/main/TransformerBattle.java
@@ -1,7 +1,70 @@
 package com.ebucher.transformers.main;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Created by buche on 11/10/2017.
+ * Takes an input of various transformers and then battles them head to head to determine a winning team
  */
 public class TransformerBattle {
+
+    private static final ArrayList<Transformer> autobots = new ArrayList<>(), decepticons = new ArrayList<>();
+    private static final ArrayList<Transformer> winners = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+        // Create and add transformers to a list
+        final ArrayList<Transformer> transformers = new ArrayList<>();
+        for (String arg : args)
+            transformers.add(new Transformer(arg));
+
+        // Sort by rank
+        Collections.sort(transformers);
+
+        // Add to team lists
+        for (Transformer transformer : transformers)
+            if (transformer.getTeam().equals("Autobots"))
+                autobots.add(transformer);
+            else
+                decepticons.add(transformer);
+
+        int battleCount = battle();
+    }
+
+    // Fight and add winners to a list, returns number of battles
+    public static int battle() {
+        int battleCount = 0;
+        while (autobots.size() > 0 && decepticons.size() > 0) {
+            winners.add(fight(autobots.get(0), decepticons.get(0)));
+            autobots.remove(0);
+            decepticons.remove(0);
+            battleCount++;
+        }
+        return battleCount;
+    }
+
+    // Returns the transformer that won the fight
+    public static Transformer fight(Transformer t1, Transformer t2) {
+        return null;
+    }
+
+    // Find who won more, returns the wins - loses for autobots
+    public static int getAutobotScore() {
+        int autobotScore = 0;
+        for (Transformer transformer : winners) {
+            if (transformer != null) {
+                if (transformer.getTeam().equals("Autobots"))
+                    autobotScore++;
+                else
+                    autobotScore--;
+            }
+        }
+        return autobotScore;
+    }
+
+    // Prints the results of the battle
+    public static void printResults() {
+
+    }
+
 }
