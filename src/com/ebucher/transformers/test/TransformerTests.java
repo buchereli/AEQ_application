b4f0e1912a878b9a484990d517755ecdb98d6710
buchereli <buchereli@gmail.com>
2017-11-12 15:37:06 -0800

    Added more comprehensive tests with some corner cases

--- src/com/ebucher/transformers/test/TransformerTests.java
+++ src/com/ebucher/transformers/test/TransformerTests.java
@@ -11,21 +11,25 @@ import org.junit.Test;
  */
 public class TransformerTests {
 
-    private Transformer soundwave, bluestreak, hubcap;
+    private Transformer soundwave, bluestreak, hubcap, optimus, predaking;
 
     @Before
     public void setUp() throws Exception {
-        TransformerBattle.main(new String[]{"Soundwave, ​D, ​8,9,2,6,7,5,6,10",
-                "Bluestreak, ​A, ​6,6,7,9,5,2,9,7",
-                "Hubcap: ​A, ​4,4,4,4,4,4,4,4"});
-        soundwave = new Transformer("Soundwave, ​D, ​8,9,2,6,7,5,6,10");
-        bluestreak = new Transformer("Bluestreak, A, 6,6,7,9,5,2,9,7");
-        hubcap = new Transformer("Hubcap: ​A, ​4,4,4,4,4,4,4,4");
+        TransformerBattle.init(new String[]{"Soundwave,D,8,9,2,6,7,5,6,10",
+                "Bluestreak,A,6,6,7,9,5,2,9,7",
+                "Hubcap:A,4,4,4,4,4,4,4,4"});
+        soundwave = new Transformer("Soundwave,D,8,9,2,6,7,5,6,10");
+        bluestreak = new Transformer("Bluestreak,A,6,6,7,9,5,2,9,7");
+        hubcap = new Transformer("Hubcap:A,4,4,4,4,4,4,4,4");
+        optimus = new Transformer("Optimus Prime,A,1,1,1,1,1,1,1,1");
+        predaking = new Transformer("Predaking,D,10,10,10,10,10,10,10,10");
     }
 
     @Test
     public void fight() throws Exception {
-        Assert.assertEquals("Wrong transformer won the fight", soundwave, TransformerBattle.fight(soundwave, bluestreak));
+        Assert.assertEquals("General fight test failed", soundwave, TransformerBattle.fight(soundwave, bluestreak));
+        Assert.assertEquals("Optimus Prime fight test failed", optimus, TransformerBattle.fight(soundwave, optimus));
+        Assert.assertEquals("Optimus Prime vs Predaking fight test failed", null, TransformerBattle.fight(predaking, optimus));
     }
 
     @Test
@@ -35,21 +39,31 @@ public class TransformerTests {
 
     @Test
     public void compareTo() throws Exception {
-        Assert.assertEquals("Error with using rank for compareTo method", -1, hubcap.compareTo(bluestreak));
-    }
-
-    @Test
-    public void getTeam() throws Exception {
-        Assert.assertEquals("Error with getTeam", "Autobots", bluestreak.getTeam());
-    }
-
-    @Test
-    public void getName() throws Exception {
-        Assert.assertEquals("Error with getName", "Bluestreak", bluestreak.getName());
+        Assert.assertTrue("Error with using rank for compareTo method", 0 < hubcap.compareTo(bluestreak));
+        Assert.assertTrue("Error with using rank for compareTo method", 0 > predaking.compareTo(bluestreak));
+        Assert.assertEquals("Error with using rank for compareTo method", 0, hubcap.compareTo(hubcap));
     }
 
     @Test
     public void transformerFight() throws Exception {
         Assert.assertEquals("Wrong transformer won the fight", 1, soundwave.fight(bluestreak));
+        Assert.assertEquals("Optimus Prime vs Predaking fight test failed", -2, predaking.fight(optimus));
+    }
+
+    @Test
+    public void print() throws Exception {
+        TransformerBattle.main(new String[]{"Soundwave,D,8,9,2,6,7,5,6,10",
+                "Bluestreak,A,6,6,7,9,5,2,9,7",
+                "Hubcap:A,4,4,4,4,4,4,4,4"});
+    }
+
+    @Test
+    public void print2() throws Exception {
+        TransformerBattle.main(new String[]{"Optimus Prime,A,1,1,1,1,1,1,1,1",
+                "Predaking,D,10,10,10,10,1,10,10,10",
+                "Soundwave,D,8,9,2,6,7,5,6,10",
+                "Cook,D,10,10,10,10,10,10,10,10",
+                "Bluestreak,A,6,6,7,9,5,2,9,7",
+                "Hubcap:A,4,4,4,4,4,4,4,4"});
     }
 }
